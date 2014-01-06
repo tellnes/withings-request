f6bddc4eb79e7d20dba68d61fbc83de91c21c128
Christian Tellnes <christian@tellnes.no>
2014-01-07 00:50:11 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,43 @@
+# withings-request <sup>[![Version Badge](http://vb.teelaun.ch/tellnes/withings-request.svg)](https://npmjs.org/package/withings-request)</sup>
+
+[![Dependency Status](https://david-dm.org/tellnes/withings-request.png)](https://david-dm.org/tellnes/withings-request)
+[![devDependency Status](https://david-dm.org/tellnes/withings-request/dev-status.png)](https://david-dm.org/tellnes/withings-request#info=devDependencies)
+[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/tellnes/withings-request/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
+
+[![NPM](https://nodei.co/npm/withings-request.png)](https://nodei.co/npm/withings-request/)
+
+
+The Withings OAuth implementation is not perfect. This lib hides that.
+
+Getting the token and token secret is outside the scope of this lib. You can use
+[passport-withings](https://github.com/mowens/passport-withings)
+to do that.
+
+Please see [withings-stream](https://github.com/tellnes/withings-stream)
+for an even higher abstraction.
+
+## Usage
+
+```js
+var options =
+  { consumerKey: '...'
+  , consumerSecret: '...'
+  , token: '...'
+  , tokenSecret: '...'
+  , userid: '...'
+  }
+var withings = require('withings-request')(options)
+
+withings('measure', 'getmeas', { startdate: 1222819200, enddate: 1223190167 }, function (err, body) {
+  if (err) throw err
+  console.log(body)
+})
+```
+
+## Install
+
+    $ npm install withings-request
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,105 @@
+var querystring = require('querystring')
+  , inherits = require('util').inherits
+  , request = require('request')
+  , http = require('http')
+  , OAuth = require('oauth').OAuth
+
+module.exports = function (options) {
+  var oauthToken = options.token
+    , oauthTokenSecret = options.tokenSecret
+    , userid = options.userid
+    , wbsUrl = options.wbsUrl || 'http://wbsapi.withings.net/'
+
+  var oauth = new OAuth(
+      '' // request_token
+    , '' // access_token
+    , options.consumerKey
+    , options.consumerSecret
+    , '1.0'
+    , '' // callback url
+    , 'HMAC-SHA1'
+    //, nonceSize
+    //, customHeaders
+    )
+
+  return function (service, action, params, cb) {
+    if (!cb) {
+      cb = params
+      params = {}
+    }
+
+    params.action = action
+    params.userid = params.userid || userid
+
+    var url = oauth.signUrl(
+        wbsUrl + service + '?' + querystring.stringify(params)
+      , oauthToken
+      , oauthTokenSecret
+      )
+
+    request(url, function (err, res, body) {
+      if (err) return cb(err)
+
+      if (res.statusCode !== 200) {
+        return cb(new Error('Unecpected HTTP Status Code: ' +
+                              res.statusCode + ' ' + http.STATUS_CODES[res.statusCode]
+                            )
+                  )
+      }
+
+      try {
+        body = JSON.parse(body)
+      } catch(err) {
+        return cb(err)
+      }
+
+      if (body.status) {
+        return cb(new WithingsError(body.status))
+      }
+
+      cb(null, body.body, res)
+    })
+  }
+}
+
+
+function WithingsError(code) {
+  Error.call(this)
+  Error.captureStackTrace(this)
+  this.name = 'WithingsError'
+  this.message =
+    [ '['
+    , String(code)
+    , '] '
+    , (WithingsError.messages[code] || 'undefined error code')
+    ].join('')
+  this.code = code
+}
+inherits(WithingsError, Error)
+module.exports.WithingsError = WithingsError
+
+WithingsError.messages =
+  {  247: 'The userid is absent or incorrect'
+  ,  250: 'The provided userid and/or Oauth credentials do not match'
+  ,  286: 'No such subscription was found'
+  ,  293: 'The callback URL is either absent or incorrect'
+  ,  294: 'No such subscription could be deleted'
+  ,  304: 'The comment is either absent or incorrect'
+  ,  305: 'Too many notifications are already set'
+  ,  342: 'The signature (using Oauth) is invalid.'
+  ,  343: 'Wrong Notification Callback Url don\'t exist'
+  ,  601: 'Too Many Request'
+  , 2554: 'Wrong action or wrong webservice'
+  , 2555: 'An unknown error occurred'
+
+
+  // https://github.com/simplificator/simplificator-withings/blob/7a08090be8a9c80fafb7f3ba1d47391c48c33ef9/lib/withings/error.rb#L6
+  ,  249: 'Called an action with invalid oauth credentials'
+
+  // I'm getting this when sending empty parameters
+  ,  246: 'Invalid parameter'
+}
+
+WithingsError.prototype.valueOf = function () {
+  return this.code
+}
